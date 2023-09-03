786225ad236591d49b29443e1090aa55b5f0ed3b
fruit765 <megafpykt@yandex.ru>
2023-09-03 05:08:17 +0300

    ddd

--- src/llm_sft.py
+++ src/llm_sft.py
@@ -132,7 +132,7 @@ def llm_sft(args: SftArguments) -> None:
     if args.model_type == 'qwen-7b':
         kwargs['use_flash_attn'] = args.use_flash_attn
     model, tokenizer, model_dir = get_model_tokenizer(
-        args.model_type, torch_dtype=torch.bfloat16, **kwargs)
+        args.model_type, torch_dtype=torch.float16, **kwargs)
 
     if args.gradient_checkpoint:
         model.gradient_checkpointing_enable()
